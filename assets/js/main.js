fef129b2192006d77ed18f21f1847c7cb834ebc4
Developer zahir <82234171+developer-zahir@users.noreply.github.com>
2023-06-25 01:17:25 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let myRoll = Number(prompt("আপনার রোল লিখুনঃ"));
+
+if (myRoll <= 10) {
+  const student = students_data.find((student) => student.roll === myRoll);
+  const { bangla, english, math, science, religion, social } = student.subjects;
+  document.getElementById("output-box").innerText = get_result(student.name, student.roll, bangla, english, math, science, religion, social);
+  document.getElementById("output-box").style.textAlign = "left";
+} else {
+  document.getElementById("output-box").innerText = `এই রোলের ডাটা আমাদের ডাটাবেজে নেই, ১ থেকে ১০ এর মধ্যে ট্রাই করুন`;
+  document.getElementById("output-box").style.background = "#c946465e";
+
+}
