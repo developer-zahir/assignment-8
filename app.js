17718fa97920742277ffe642109f45c3fdad9d8f
Developer zahir <82234171+developer-zahir@users.noreply.github.com>
2023-06-25 01:17:00 +0600

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-let myRoll = Number(prompt("Enter your roll:"));
-if(myRoll <= 10 ){
-    const student = students_data.find((student) => student.roll === myRoll);
-    const { bangla, english, math, science, religion, social } = student.subjects;
-    console.log(get_result(student.name, student.roll, bangla, english, math, science, religion, social));
-}else{
-    console.log(`এই রোলের ডাটা আমাদের ডাটাবেজে নেই, ১ থেকে ১০ এর মধ্যে ট্রাই করুন`);
-}
